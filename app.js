d50ad686df1de2b6171a0233a132273b0dd31a16
Arnav Dhiman <arnav.dhiman@gmail.com>
2021-05-27 20:48:35 -0700

    adds test

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const app = express();
 const mintRoute = require('./routes/mint-nft') 
 const { json } = require('body-parser');
 var cors = require('cors')
-
+const testRoute = require("./routes/test");
 //Middlewares
 app.use(cors());
 //Parse json req.body
 app.use(json());
 //minting NFT middleware
 app.use('/mint', mintRoute);
-
+app.use('/test', testRoute)
 // start listening to server
 // port # = 9000
 app.listen(9000);
